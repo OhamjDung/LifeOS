df9b477d0ad4b6582f2d6890ea987632b0609c65
Tom Pham <ohamjdung196@gmail.com>
2026-06-10 15:14:45 +0700

    d

--- mobile/index.ts
+++ mobile/index.ts
@@ -1,1 +1,12 @@
-import 'expo-router/entry'
+const { Alert } = require('react-native')
+
+global.ErrorUtils?.setGlobalHandler?.((error: any, isFatal?: boolean) => {
+  if (isFatal) {
+    Alert.alert(
+      'Fatal JS Error',
+      (error?.message ?? 'no message') + '\n\n' + (error?.stack?.slice(0, 500) ?? 'no stack')
+    )
+  }
+})
+
+require('expo-router/entry')
